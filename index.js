b527cd9b939a3336688ad9a3f53248963b1c18db
Sweta Das <swetadas570@gmail.com>
2026-05-14 01:28:49 +0530

    Fixed Vercel deployment configuration

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import axios from "axios";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
